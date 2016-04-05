9be16e49fd59bcfdc790560d4732665e7ceb8461
simonla <804150485@qq.com>
2016-04-05 21:32:55 +0800

    增加失败重新加载

--- app/src/main/java/com/gank/simonla/gank/utils/PhotoLibrary/PhotoLoader.java
+++ app/src/main/java/com/gank/simonla/gank/utils/PhotoLibrary/PhotoLoader.java
@@ -22,6 +22,7 @@ import java.net.URL;
 /**
  * Created by Simonla on 2016/3/28.
  */
+/*
 public class PhotoLoader {
 
     public static final String TAG = "PhotoLoader";
@@ -182,6 +183,185 @@ public class PhotoLoader {
         } catch (IOException e) {
             e.printStackTrace();
             //TODO 获取异常种类进行处理
+        }
+        return null;
+    }
+
+    public interface DrawableCallbackListener {
+        void onBitmapFinish(Bitmap response);
+
+        void onError(Exception e);
+    }
+
+}
+*/
+public class PhotoLoader {
+
+    public static final String TAG = "PhotoLoader";
+    private static boolean sIsFailTouchToReload = false;
+    private static Context sContext;
+    private static int sResLoad;
+    private static int sResFail;
+    private static int sCompressionRatio = 100;
+
+    public static void setIsFailTouchToReload(boolean isFailTouchToReload) {
+        sIsFailTouchToReload = isFailTouchToReload;
+    }
+
+    public static void init(Context context) {
+        sContext = context;
+    }
+
+    public static void setLoadDefault(int res) {
+        sResLoad = res;
+    }
+
+    public static void setFailDefault(int res) {
+        sResFail = res;
+    }
+
+    public static void setCompressionRatio(int compressionRatio) {
+        sCompressionRatio = compressionRatio;
+    }
+
+    // 重载一个不要外部处理的，就和你原来一样了
+    public static void open(String url, ImageView iv) {
+        open(url, iv, null);
+    }
+
+    // 你应该放一个回调出去……否则下载完了我想干点别的事情都不行了
+    public static void open(final String url, final ImageView iv, final DrawableCallbackListener listener) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getHttpBitmap(url, iv,new DrawableCallbackListener() {
+                    @Override
+                    public void onBitmapFinish(final Bitmap response) {
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                // 如果tag对应上了
+                                if (iv.getTag() == url) {
+                                    iv.setImageBitmap(response);
+                                    // 让外面也可以自己添加额外的操作
+                                    if (listener != null) listener.onBitmapFinish(response);
+                                }
+                            }
+                        });
+                    }
+
+                            @Override
+                            public void onError(final Exception e) {
+                                e.printStackTrace();
+                                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        iv.setImageResource(sResFail);
+                                        reLoad();
+                                    }
+
+                                    private void reLoad() {
+                                        if (sIsFailTouchToReload) {
+                                            iv.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    open(url, iv);
+                                                    Toast.makeText(sContext, "请检查网络", Toast.LENGTH_SHORT).show();
+                                                    Log.d(TAG, "onClick: " + e);
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                );
+            }
+        }).start();
+    }
+
+    private static Bitmap getHttpBitmap(final String address, final ImageView iv, final DrawableCallbackListener listener) {
+        if (address == null) {
+            iv.setImageResource(sResLoad);
+        } else {
+            final int a = address.hashCode();
+            if (getBitmapFromNative(a) != null) {
+                listener.onBitmapFinish(getBitmapFromNative(a));
+            } else {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        iv.setImageResource(sResLoad);
+                    }
+                });
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(address);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setUseCaches(true);
+                    connection.setConnectTimeout(4000);
+                    connection.setReadTimeout(4000);
+                    InputStream in = connection.getInputStream();
+                    Bitmap response = BitmapFactory.decodeStream(in);
+                    saveFile(a, response);
+                    in.close();
+                    if (listener != null) {
+                        listener.onBitmapFinish(response);
+                    }
+                } catch (Exception e) {
+                    if (listener != null) {
+                        listener.onError(e);
+                    }
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private static void saveFile(int name, Bitmap bitmap) {
+        FileOutputStream outputStream = null;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, sCompressionRatio, out);
+        byte[] result = out.toByteArray();
+        try {
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            outputStream = sContext.openFileOutput(String.valueOf(name), Context.MODE_PRIVATE);
+            outputStream.write(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Bitmap getBitmapFromNative(int name) {
+        FileInputStream in = null;
+        try {
+            in = sContext.openFileInput(String.valueOf(name));
+            return BitmapFactory.decodeStream(in);
+        } catch (Exception ignored) {
+        }
+        try {
+            if (in != null) {
+                in.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
--- app/src/main/java/com/gank/simonla/gank/view/adapter/GirlsAdapter.java
+++ app/src/main/java/com/gank/simonla/gank/view/adapter/GirlsAdapter.java
@@ -1,11 +1,15 @@
 package com.gank.simonla.gank.view.adapter;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +18,8 @@ import com.gank.simonla.gank.bean.Girls;
 import com.gank.simonla.gank.utils.PhotoLibrary.PhotoLoader;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by asus on 2016/4/3.
@@ -22,6 +28,9 @@ public class GirlsAdapter extends RecyclerView.Adapter<GirlsAdapter.ViewHolder> 
 
     private ArrayList<Girls.ResultsBean> mGirlsList;
     private OnItemClickListener mOnItemClickListener;
+    // 存放图片的比例
+    private Map<String, Double> mPicRatios = new HashMap<>();
+    private int screenWidth;
 
     public GirlsAdapter(ArrayList<Girls.ResultsBean> girlsList) {
         mGirlsList = girlsList;
@@ -29,6 +38,8 @@ public class GirlsAdapter extends RecyclerView.Adapter<GirlsAdapter.ViewHolder> 
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 这里得到一下屏幕的宽度，不优雅，但我是不想写Context了，直接这里获取
+        screenWidth = getScreenWidth(parent.getContext());
         return new ViewHolder(LayoutInflater.
                 from(parent.getContext()).
                 inflate(R.layout.item_girls_list, parent, false));
@@ -36,15 +47,43 @@ public class GirlsAdapter extends RecyclerView.Adapter<GirlsAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
+        final String url = mGirlsList.get(position).getUrl();
         CardView cardView = holder.mCardView;
-        ImageView photo = holder.mPhoto;
-        PhotoLoader.open(mGirlsList.get(position).getUrl(), photo);
+        final ImageView photo = holder.mPhoto;
+        // 其实你的顺序还是会错乱的，这里还是给iv一个tag
+        photo.setTag(url);
+        // 给iv清空图片，否则看到图片变动感觉和跳加载错乱跳动一样
+        photo.setImageResource(R.drawable.ic_loading);
+
+        // 如果事先保存了图片的比例就给iv设置相应的高度
+        if (mPicRatios.get(url) != null) {
+            FrameLayout.LayoutParams rlp = (FrameLayout.LayoutParams) photo.getLayoutParams();
+            // 这里为什么用屏幕的宽度/2, 不用rlp.width呢？因为你cardView的width是MATCH_PARENT
+            // 并不是一个具体值，返回的是 -1, 当滑动非常快的时候还是会有一点item移动
+            // 为什么？因为iv的宽小于屏幕/2, cardView那家伙还有边距的嘛，这个我就不算了
+            rlp.height = (int) (mPicRatios.get(url) * screenWidth / 2);
+            holder.mPhoto.setLayoutParams(rlp);
+        }
+
+        PhotoLoader.open(url, photo, new PhotoLoader.DrawableCallbackListener() {
+            @Override
+            public void onBitmapFinish(Bitmap response) {
+                // 当下载完bitmap，把它的比例给记下来
+                mPicRatios.put(url, response.getHeight() / (double) response.getWidth());
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+
         photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             }
         });
-       // name.setText(girl.getWho());
+        // name.setText(girl.getWho());
     }
 
     @Override
@@ -72,5 +111,11 @@ public class GirlsAdapter extends RecyclerView.Adapter<GirlsAdapter.ViewHolder> 
 
     public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
         mOnItemClickListener = onItemClickListener;
+    }
+
+    // 这个工具我就先给你写在这里吧
+    public static int getScreenWidth(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return dm.widthPixels;
     }
 }
